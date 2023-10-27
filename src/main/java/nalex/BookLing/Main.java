edcbc4240c9d1f22cca490f39608c34e8eb6778c
Alex <eBSvwX8xBmmx9k2V@gmail.com>
2023-10-27 10:23:38 +0200

    Added swing to the project

--- src/main/java/nalex/BookLing/Main.java
+++ src/main/java/nalex/BookLing/Main.java
@@ -4,6 +4,7 @@ import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Metadata;
 import nl.siegmann.epublib.epub.EpubReader;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -17,6 +18,17 @@ public class Main {
         EpubReader epubReader = new EpubReader();
         Book testBook = epubReader.readEpub(targetStream);
         Metadata metadata = testBook.getMetadata();
+
+        JFrame f=new JFrame();//creating instance of JFrame
+
+        JTextField testField = new JTextField("kekw");
+
+        testField.setBounds(130,100,100, 40);
+        f.add(testField);
+
+        f.setSize(400,500);//400 width and 500 height
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
 
         System.out.println(metadata.getAuthors().toString());
     }
