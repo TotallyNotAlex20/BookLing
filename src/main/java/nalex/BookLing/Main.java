c3e700b4e3b733fae8958235d32b8d671dc6d00b
Alex <eBSvwX8xBmmx9k2V@gmail.com>
2023-10-28 21:06:01 +0200

    Added FlatLaf to the project

--- src/main/java/nalex/BookLing/Main.java
+++ src/main/java/nalex/BookLing/Main.java
@@ -1,32 +1,55 @@
 package nalex.BookLing;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Metadata;
 import nl.siegmann.epublib.epub.EpubReader;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Main {
+
+    public static int window_width = 500;
+    public static int window_height = 600;
+
     public static void main(String[] args) throws IOException {
+
+        try {
+            UIManager.setLookAndFeel( new FlatLightLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
+        // Using this process to invoke the constructor,
+        // JFileChooser points to user's default directory
+        JFileChooser j = new JFileChooser();
+
+        // Open the save dialog
+        j.showSaveDialog(null);
+
+
         File initialFile = new File("src/main/resources/test123.epub");
         InputStream targetStream = new FileInputStream(initialFile);
+
 
         EpubReader epubReader = new EpubReader();
         Book testBook = epubReader.readEpub(targetStream);
         Metadata metadata = testBook.getMetadata();
 
-        JFrame f=new JFrame();//creating instance of JFrame
+        JFrame f=new JFrame();
 
-        JTextField testField = new JTextField("kekw");
+        System.out.println(testBook.getContents().toString());
+        JTextComponent testField = new JTextArea(testBook.getContents().toString()); // get the author out of the list, so it looks nice
 
-        testField.setBounds(130,100,100, 40);
+        testField.setBounds(0,0, window_width, window_height);
+        testField.setEditable(false);
         f.add(testField);
 
-        f.setSize(400,500);//400 width and 500 height
+        f.setSize(window_width, window_height);
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
 
